a23c47db95de1a5e7998639560650b162afe177e
lucaspedraocruz <167530609+lucaspedraocruz@users.noreply.github.com>
2026-05-11 08:34:44 -0300

    wg

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ botoes[i].onclick = function(){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-5-01T00:20:40");
-const tempoObjetivo2 = new Date("2026-20-08T00:10:23");
+const tempoObjetivo1 = new Date("2026-6-01T00:20:40");
+const tempoObjetivo2 = new Date("2026-10-08T00:10:23");
 const tempoObjetivo3 = new Date("2026-11-15T00:01:43");
-const tempoObjetivo4 = new Date("2026-30-01T00:01:01");
+const tempoObjetivo4 = new Date("2026-12-01T00:01:01");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
